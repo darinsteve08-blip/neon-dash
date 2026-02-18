624c059b8d0c6eea3fe80e8c89eefadbf168bce0
darinsteve08-blip <darinsteve08@gmail.com>
2026-02-18 18:53:23 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = "neon-dash-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
